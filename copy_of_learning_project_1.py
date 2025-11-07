96b3d6100ca59b16d5b9babb360a763e6c0b6692
kadelsahas9-lang <kadelsahas9@gmail.com>
2025-11-07 19:18:37 +0530

    Update copy_of_learning_project_1.py

--- copy_of_learning_project_1.py
+++ copy_of_learning_project_1.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1IrtPSnMjWNORzXa9CxAjC7PEkoWEaI9W
 """
 
-!pip install streamlit
+pip install streamlit
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile app.py
